a6c4702ebc6a91564549e107df98baf91eb88d67
DipuBarua <you@example.com>
2023-10-20 00:33:17 +0600

    updated get operation for same branded product's individual page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -10,9 +10,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm0qnz4.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,14 +28,35 @@ async function run() {
 
         const fashionCollection = client.db("fashionDB").collection("fashion");
 
-
+        //  get for all data 
         app.get('/fashion', async (req, res) => {
             const cursor = fashionCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
+        // sigle data get fot other purposes 
+        app.get("/fashion/:brand_name", async (req, res) => {
+            const brand_name = req.params.brand_name;
+            const query = { brand_name: brand_name };
+            const search = fashionCollection.find(query);
+            // const file = (search) => {
+            //     if (!search || search.length === 0) {
+            //         return ('no file exist.')
+            //     }
+            //     else {}
+            // }
+            const result = await search.toArray();
+            res.send(result);
 
+        })
+
+        app.post('/fashion', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await fashionCollection.insertOne(newProduct);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
